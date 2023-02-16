5ce7edaf4bf5a8b78cb6e763ec3146535c564153
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2023-02-16 18:02:52 +0100

    going to code trident dings

---
+++ src/main/java/net/fabricmc/example/mixin/entity/projectile/TridentEntityMixin.java
@@ -0,0 +1,21 @@
+package net.fabricmc.example.mixin.entity.projectile;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TridentEntity.class)
+public abstract class TridentEntityMixin extends PersistentProjectileEntity {
+    protected TridentEntityMixin(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        System.out.println("Nothing " + blockHitResult);
+    }
+}
