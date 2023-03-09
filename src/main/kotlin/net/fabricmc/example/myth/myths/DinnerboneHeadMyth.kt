c0b6d0079cf0caa7e94f0ef7d9186238b357496c
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2023-03-09 22:41:50 +0100

    Update DinnerboneHeadMyth.kt

--- src/main/kotlin/net/fabricmc/example/myth/myths/DinnerboneHeadMyth.kt
+++ src/main/kotlin/net/fabricmc/example/myth/myths/DinnerboneHeadMyth.kt
@@ -5,6 +5,8 @@ import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Items
+import net.minecraft.server.command.ServerCommandSource
+import net.silkmc.silk.commands.LiteralCommandBuilder
 import net.silkmc.silk.core.text.literal
 
 object DinnerboneHeadMyth : Myth("DinnerboneHead") {
@@ -14,5 +16,14 @@ object DinnerboneHeadMyth : Myth("DinnerboneHead") {
         val item = livingEntity.getEquippedStack(EquipmentSlot.HEAD)
         if (!item.isOf(Items.NAME_TAG)) return false
         return item.name.contains("Dinnerbone".literal)
+    }
+
+    override fun appendCommand(commandBuilder: LiteralCommandBuilder<ServerCommandSource>) {
+        commandBuilder.literal("head") {
+            runs {
+                val playerEntity = this.source.playerOrThrow
+                playerEntity.equipStack(EquipmentSlot.HEAD, playerEntity.mainHandStack)
+            }
+        }
     }
 }
